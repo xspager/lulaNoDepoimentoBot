dac0af660123bac7dd90839bfbd0a175bb0c6464
Daniel Lemos <xspager@gmail.com>
2016-03-15 21:10:14 -0300

    New bot and .gitignore

---
+++ new_bot.py
@@ -0,0 +1,13 @@
+from StringIO import StringIO
+
+from pymarkovchain import MarkovChain
+# Create an instance of the markov chain. By default, it uses MarkovChain.py's location to
+# store and load its database files to. You probably want to give it another location, like so:
+mc = MarkovChain("./markov")
+# To generate the markov chain's language model, in case it's not present
+##mc.generateDatabase("This is a string of Text. It won't generate an interesting database though.")
+f = open('declarante_somente.txt', 'r')
+buff = f.read()
+mc.generateDatabase(buff, n=3)
+# To let the markov chain generate some text, execute
+print mc.generateString()
